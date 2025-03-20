970bc06deb6e55d2a86db0843d43ec20d7254f96
kaiwen.ku <kaiwen.ku@synergydesigntek.com>
2025-03-20 17:48:26 +0800

    沒有delete動態配置的空間

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#define ARRAY_SIZE (10)
 using namespace std;
 
 int main()
 {
-    cout << "Test!" << endl;
+    int *iHead = new int[ARRAY_SIZE];
+    int *p = iHead;
+
+    for (int i = 0; i < 10; i++, p++)
+    {
+        *p = i;
+    }
+
+    p = iHead;
+    for (int i = 0; i < 10; i++, p++)
+    {
+        cout << *p << endl;
+    }
+
+    cout << "Test finished!" << endl;
     return 0;
 }
